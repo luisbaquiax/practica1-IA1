e500a74c09c0491403d1859dfdf27eff907cad3c
luisbaquiax <luis.baquiax1995@gmail.com>
2026-04-11 04:22:40 -0600

    update: config ga

--- ApiSistemaHorario/src/AlgoritmoGenetico/types/ConfigGA.type.ts
+++ ApiSistemaHorario/src/AlgoritmoGenetico/types/ConfigGA.type.ts
@@ -1,8 +1,13 @@
 export interface ConfigGA {
-  tamanioPoblacion: number;    // ej: 100
-  maxGeneraciones: number;     // ej: 200
-  tasaMutacion: number;        // 0.0 – 1.0
-  tasaCruce: number;           // 0.0 – 1.0
-  elitismo: number;            // numero de individuos élite que pasan directo
-  maxCursosPorHorario: number; // límite de cursos a asignar
+    tamanioPoblacion: number;    // ej: 100
+    maxGeneraciones: number;     // ej: 200
+    tasaMutacion: number;        // 0.0 – 1.0
+    tasaCruce: number;           // 0.0 – 1.0
+    elitismo: number;            // numero de individuos élite que pasan directo
+    maxCursosPorHorario: number; // límite de cursos a asignar
+
+    // Métodos elegidos por el usuario
+    metodoSeleccion: "torneo" | "ruleta";
+    metodoCruce: "un_punto" | "multipunto" | "mascara_aleatoria";
+    metodoMutacion: "intercambio" | "random_resetting";
 }
